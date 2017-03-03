12031287f5c465927b3cf7dad672bd4348a45e24
cipriancus <cipriancus@gmail.com>
2017-03-03 15:31:17 +0200

    Implementat libraria
    Am implementat desc LU si LDLT folosind numpy , am cautat solutia la
    sistem si am calculat norma, mai ramane CITIREA de la tastatura,
    verificarea si interfata

--- Tema2/ex.py
+++ Tema2/ex.py
@@ -1,6 +1,11 @@
+from itertools import count
+import copy
+
 a = [[1, -1, 2],
      [-1, 5, -4],
      [2, -4, 6]]
+
+copyOfA=copy.deepcopy(a)
 
 b = [1, 0, 1]
 
@@ -72,3 +77,39 @@ print("x Chol este:", x)
 # citit de la tastatuta conform problemei
 #   (mai bine sa o faci la sfarsit ca sa nu trebuiasca sa tot
 #    introduci date cand testezi)
+
+#-----------------------LDLT cu numpy----------------------------------
+import numpy
+print('-------------------------LIBRARIA------------------------')
+
+A=numpy.array(copyOfA)
+L=numpy.linalg.cholesky(A)#descompunerea LLT
+
+#Pentru a forma LDLT din LLT
+#folosim un S care are diagonala din L
+
+S=numpy.zeros((n,n))
+
+for i in range(0,n):#parcurgem diag L
+    S[i][i]=L[i][i]
+
+D=numpy.dot(S,S)#D=S^2
+L=numpy.dot(L,numpy.linalg.inv(S))#L=L*S^-1
+
+print('D este',D)
+print('L este',L)
+print('Inmultirea da ',numpy.dot(numpy.dot(L,D),L.T.conj()))
+
+#----------------------DESCOMPUNEREA LU-----------------------
+
+A=numpy.array(copyOfA)
+L=numpy.linalg.cholesky(A)
+print('Descompunerea LU cu L: ',L)#descompunerea LU, daca A e sim si poz definita
+
+#-------------------------Solutia sistemului -----------------
+x=numpy.linalg.solve(A,b)
+print('Solutia librariei pentru sistem este: ',x)
+
+#-------------------------Norma-------------------------------
+A=numpy.subtract(numpy.dot(A,x),b)#A*x-b
+print('Norma este: ',numpy.linalg.norm(A,ord=2))#calc norma 2, sqrt(sum(abs(xi)^2))
