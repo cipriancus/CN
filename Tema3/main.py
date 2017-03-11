b70e16fcaa84af4f7beb0c321035087e7f2e79b2
AsoFC <asofiei.cosmina@yahoo.com>
2017-03-11 23:40:57 +0200

    My try.

--- Tema3/main.py
+++ Tema3/main.py
@@ -61,6 +61,13 @@ val_aplusb = [float(e) for e in f.readline().strip("[").strip("]\n").split(", ")
 col_aplusb = [float(e) for e in f.readline().strip("[").strip("]\n").split(", ")]
 b_aplusb = [float(e) for e in f.readline().strip("[").strip("]\n").split(", ")]
 
+f = open("output_transpus_b.txt")
+n_b_t = int(f.readline())
+d_b_t = [float(e) for e in f.readline().strip("[").strip("]\n").split(", ")]
+val_b_t = [float(e) for e in f.readline().strip("[").strip("]\n").split(", ")]
+col_b_t = [float(e) for e in f.readline().strip("[").strip("]\n").split(", ")]
+b_b_t = [float(e) for e in f.readline().strip("[").strip("]\n").split(", ")]
+
 x = [n_a - i + 1 for i in range(1, n_a + 1)]
 
 
@@ -113,50 +120,97 @@ def aorix(d, val, col, b, name):
                                val[col.index(iterator) + 1:col.index(iterator - 1)]):
             elem_b += elem * x[int(index) - 1]
         b_aux.append(elem_b)
-        if b[-iterator - 1] - b_aux[-iterator - 1] > epsilon:
+        if abs(b[-iterator - 1] - b_aux[-iterator - 1]) > epsilon:
             identical = False
             print(b[-iterator - 1], b_aux[-iterator - 1], -iterator - 1)
         iterator -= 1
     print("\tCalculul pt " + name + "*x este corect:", identical)
+    return b_aux
 
-def aorib(d_a, val_a, col_a,d_b,val_b,col_b,a_ori_b):
+
+def memorare_economica_transpus(nume_fis):
+    print("Se memoreaza valorile din fisierul", nume_fis, ".")
+    n = int(open(nume_fis).readline())
+    d = [0.0 for i in range(n + 1)]
+    val = [0.0 for i in range(n + 1)]
+    col = [-i - 1 for i in range(n + 1)]
+    b = list()
+    ok = 0
+    for line in open(nume_fis):
+        if "," in line:
+            elem, x, y = line.split(",")
+            if float(x) == float(y):
+                d[int(x)] = float(elem)
+            else:
+                col.insert(col.index(-int(y) - 1) + 1, float(x) + 1)
+                val.insert(col.index(-int(y) - 1) + 1, float(elem))
+
+        else:
+            aux = line.strip("\n")
+            if len(aux) > 1 and ok == 1:
+                b.append(float(aux))
+            ok = 1
+    print(d)
+    print(val)
+    print(col)
+    open("output_transpus_" + nume_fis, "wt").write(str(n) + "\n")
+    open("output_transpus_" + nume_fis, "a").write(str(d) + "\n")
+    open("output_transpus_" + nume_fis, "a").write(str(val) + "\n")
+    open("output_transpus_" + nume_fis, "a").write(str(col) + "\n")
+    open("output_transpus_" + nume_fis, "a").write(str(b) + "\n")
+
+
+def aorib():
     print("\nSe calculeaza A*B si se verifica daca rezultatul\neste identic cu cel din fisier:")
     identical = True
 
-    d = [0.0 for i in range(n_a + 1)]
+    d = list()
     val = [0.0 for i in range(n_a + 1)]
     col = [-i - 1 for i in range(n_a + 1)]
 
-    coloana= 1
-    while coloana <n_a:
-        linie = -1
-        while -linie < n_a:
-            lista_val_a = list()
-            lista_col_a = list()
+    #linie in matricea rezultat; in loc de -10 ar tb sa fie -n_a
+    for linie in range(-1, -10, -1):
 
-            line_a = col_a[col_a.index(linie) + 1:col_a.index(linie - 1)]
+        #calcul matrice
+        print(linie)
+        # element in matricea rezultat
+        for coloana in range(-1,-n_a, -1):
+            elem_sum = 0
 
-            for i in range(0, len(line_a)):
-                lista_val_a.append(val_a[col_a.index(linie) + 1 + i])
-                lista_col_a.append(line_a[i])
+            # linie din matricea A memorata economic
+            for index_a, elem_a in zip(col_a[col_a.index(linie) + 1:col_a.index(linie - 1)],
+                                       val_a[col_a.index(linie) + 1:col_a.index(linie - 1)]):
 
-            for iterator in lista_col_a:
-                iterator=int(-iterator)
-                lista_val_b = list()
-                lista_col_b = list()
-
-                line_b= col_b[col_b.index(iterator)+1:col_b.index(iterator - 1)]
-
-                for i in range(0, len(line_b)):
-                    lista_val_b.append(val_b[col_b.index(iterator) + 1 + i])#iau toate liniile ce au numarul ca si coloana din A
-                    lista_col_b.append(line_b[i])
-
-                if len(lista_val_b)!=0 and coloana in lista_col_b:#inseamna ca avem elemente cu ce inmulti
-                    a=2
-
-
-            linie=linie-1
-        coloana=coloana+1
+                try:
+                    # unele elemente trebuie inmultite cu elemente de pe diaginala
+                    if index_a == -coloana-1:
+                        elem_sum += elem_a * d_b[-coloana-1]
+                    else:
+                        # pentru celelaltele, cautam elementul din linia lui B transpus de pe aceeasi coloana
+                        index_b = index_a
+                        elem_b = val_b_t[col_b_t[col_b_t.index(coloana) + 1:col_b_t.index(coloana - 1)].index(index_b)]
+                        if index_b == -linie-1:
+                            elem_sum += elem_b * d_a[-linie-1]
+                        else:
+                            elem_sum += elem_b * elem_a
+                except Exception as e:
+                    pass
+            # acest if functioneaza, diagonala e calculata corect
+            if linie == coloana:
+                elem_sum += d_a[-linie - 1] * d_b[-coloana - 1]
+                d.append(elem_sum)
+                if abs(d[-linie - 1] - d_aorib[-linie - 1]) > epsilon:
+                    identical = False
+            # acest else nu ar trebui sa afecteze rezultatul
+            else:
+                if elem_sum > epsilon:
+                    col.insert(col.index(linie) + 1, -coloana)
+                    val.insert(col.index(linie) + 1, elem_sum)
+    # cand le afisez, nu au elemente comune de la -1 pana la -2
+    print(val)
+    print(val_aorib)
+    print(col)
+    print(col_aorib)
 
 
 # decomenteaza pt a calcula
@@ -166,8 +220,10 @@ def aorib(d_a, val_a, col_a,d_b,val_b,col_b,a_ori_b):
 # memorare_economica("b.txt")
 # memorare_economica("aorib.txt")
 # memorare_economica("aplusb.txt")
+# memorare_economica_transpus("b.txt")
 
-#a_plus_b()
-#aorix(d_a, val_a, col_a, b_a, "A")
-#aorix(d_b, val_b, col_b, b_b, "B")
-aorib(d_a,val_a,col_a,d_b,val_b,col_b,b_aorib)
+# a_plus_b()
+# aorix(d_a, val_a, col_a, b_a, "A")
+# aorix(d_b, val_b, col_b, b_b, "B")
+
+aorib()
